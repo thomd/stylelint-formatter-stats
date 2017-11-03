59958d7c01eae7855af1e1709f6ff550492c1f1d
Thomas Dürr <thomas.duerr@arvato.com>
2017-11-04 00:36:02 +0100

    bars

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const flatMap = require('lodash/flatMap')
 const groupBy = require('lodash/groupBy')
 const mapValues = require('lodash/mapValues')
+const bar = require('./lib/bar')
 
 const formatter = results => {
   var stats = mapValues(groupBy(flatMap(results, 'warnings'), 'rule'), count => count.length)
-  return require('util').inspect(stats)
+  return bar(stats)
 }
 
 module.exports = formatter
