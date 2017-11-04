44588adf9c1688cc330f5110e30ebd2c246cebed
Thomas Duerr <thomduerr@gmail.com>
2017-11-04 23:53:55 +0100

    code cleanup

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const groupBy = require('lodash/groupBy')
 const mapValues = require('lodash/mapValues')
 const bar = require('./lib/bar')
 
-const formatter = results => {
-  var stats = mapValues(groupBy(flatMap(results, 'warnings'), 'rule'), count => count.length)
-  return bar(stats)
-}
+const formatter = results => bar(mapValues(groupBy(flatMap(results, 'warnings'), 'rule'), count => count.length))
 
 module.exports = formatter
